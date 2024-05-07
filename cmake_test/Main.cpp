28333b17cc3da6d35f4f8c30bbb76ac798e32ebd
DESKTOP-34FVBVG\lirong <2229608151@qq.com>
2024-05-07 22:57:43 +0800

    2024.5.7

--- cmake_test/Main.cpp
+++ cmake_test/Main.cpp
@@ -1,10 +1,13 @@
 
 #include <iostream>
-
+#include "add.h"
 #define AA NULL
 
+using namespace std;
 int main(int argc, const char **argv)
 {
-    std::cout << AA << std::endl;
-    std::cout << 2 << std::endl;
+
+    // std::cout << AA << std::endl;
+    // std::cout << 2 << std::endl;
+    cout << add(1, 2) << endl;
 }
