38c7ad92cb82e415cdecff21412309e11fd0361a
DESKTOP-34FVBVG\lirong <2229608151@qq.com>
2024-05-09 23:15:13 +0800

    提交

---
+++ cmake_test/build/CMakeFiles/hao.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hao.dir/Main.cpp.obj"
+  "CMakeFiles/hao.dir/Main.cpp.obj.d"
+  "hao.exe"
+  "hao.exe.manifest"
+  "hao.pdb"
+  "libhao.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hao.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
