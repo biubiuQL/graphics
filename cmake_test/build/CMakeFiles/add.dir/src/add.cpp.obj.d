f74ea54de2d21879cfa16e4a17936833b977be8d
DESKTOP-3FFMDEV\admin <2229608151@qq.com>
2024-09-09 10:48:40 +0800

    删除cmake_test

--- cmake_test/build/CMakeFiles/add.dir/src/add.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/add.dir/src/add.cpp.obj: \
- C:\Users\admin\Documents\graphics\cmake_test\src\add.cpp
--- cmake_test/build/Math/CMakeFiles/Math.dir/src/sub.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-Math/CMakeFiles/Math.dir/src/sub.cpp.obj: \
- C:\Users\admin\Documents\graphics\cmake_test\Math\src\sub.cpp
